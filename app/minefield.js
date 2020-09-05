dc93abdb06d49dc9122b3496debff81f2cbc8e24
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-09-05 10:25:44 -0300

    styled minefield

--- app/minefield.js
+++ app/minefield.js
@@ -1,7 +1,55 @@
+const BOX = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven'
+};
+
 const FIELD_SIZE = 500;
 
-const start = () => {
-  [...new Array(FIELD_SIZE)].forEach(() => {
-    document.write('<div class="box one">1</div>');
-  });
+const random = () => Math.floor(Math.random() * 8);
+
+// Reveal a box
+const reveal = ({ target }) => {
+  const value = target.getAttribute('value');
+  target.className = `box ${ BOX[value] }`;
+  // We don't want to show 0
+  if (value > 0) {
+    target.innerHTML = value;
+  }
 }
+
+// Create a random box
+const getRandomBox = () => {
+  const value = random();
+  const div = document.createElement('div');
+
+  // Start as closed
+  div.setAttribute('open', false);
+  // Set the inner value
+  div.setAttribute('value', value);
+  // Set the onclick event that reveals a box
+  div.setAttribute('onclick', 'reveal(event)')
+  // Set the className
+  div.className = 'box';
+
+  return div;
+};
+
+const start = ({ target }) => {
+  // Hide the start button
+  target.hidden = true;
+
+  // Get the field element
+  const field = document.getElementById('field');
+
+  // Populates the field
+  [...new Array(FIELD_SIZE)].forEach(() => {
+    const child = getRandomBox();
+    field.appendChild(child);
+  });
+};
